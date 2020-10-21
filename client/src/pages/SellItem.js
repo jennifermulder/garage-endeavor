62718fe7aef1b0e4c7a4fd5ab8184ab71d9aee6c
shhu21 <hu.sara21@gmail.com>
2020-10-21 16:53:10 -0700

    queries to update the product list (broken)

--- client/src/pages/SellItem.js
+++ client/src/pages/SellItem.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
+import { QUERY_PRODUCTS } from "../utils/queries";
+import { useQuery } from '@apollo/react-hooks';
+import { idbPromise } from "../utils/helpers";
+import { useStoreContext } from '../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../utils/actions';
 
 const SellItem = () => {
-    const [formState, setFormState] = useState({ title: '', price: '', description: '', image: '' });
+    const [formState, setFormState] = useState({ title: '', category: '', price: '', description: '', image: '' });
+    const { loading, data } = useQuery(QUERY_PRODUCTS);
+    const [state, dispatch] = useStoreContext();
 
     const handleFormSubmit = async event => {
         event.preventDefault();
@@ -9,8 +16,30 @@ const SellItem = () => {
         // if(formState.title && formState.price && formState.description && formState.image) {
 
         // }
-        if(formState.title && formState.price && formState.description) {
-            console.log(`${formState.title}, ${formState.price}, ${formState.description}`);
+        if(formState.title && formState.category && formState.price && formState.description) {
+            console.log(`${formState.title}, ${formState.category}, ${formState.price}, ${formState.description}`);
+
+            if (data) {
+                // FIXME: not data.products isn't coming through
+                const newProductList = [...data.products, formState];
+                console.log('data', data.products)
+                console.log(newProductList)
+                dispatch({
+                  type: UPDATE_PRODUCTS,
+                  products: newProductList
+                });
+            
+                newProductList.forEach((product) => {
+                  idbPromise('products', 'put', product);
+                });
+              } else if (!loading) {
+                idbPromise('products', 'get').then((products) => {
+                  dispatch({
+                    type: UPDATE_PRODUCTS,
+                    products: products
+                  });
+                });
+            }
         }
         else{
             console.log("error");
@@ -33,6 +62,12 @@ const SellItem = () => {
             <input 
                 name='title'
                 placeholder='Add a title'
+                onChange={handleChange}
+            />
+            <label>Category</label>
+            <input 
+                name='category'
+                placeholder='Add a category'
                 onChange={handleChange}
             />
             <label>Price</label>
