d8e09fdd823db076371689f338fd6fce1e4d03d7
sitakura <scot.itakura@gmail.com>
2020-10-22 12:56:25 -0700

    added some styling to signup and login pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+
+import styled from "styled-components"
+import loginPattern from "../assets/images/login-background.jpg"
+
 import { useMutation } from '@apollo/react-hooks';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { LOGIN } from "../utils/mutations"
 import Auth from "../utils/auth";
 
@@ -27,46 +31,74 @@ function Login(props) {
     });
   };
 
-  return (
-    <div className="container my-1">
-      <Link to="/signup">
-        ← Go to Signup
-      </Link>
+  const StyledLoginBackground = styled.div`
+    background-image: url(${loginPattern});
+    position: fixed;
+    height: 100vh;
+    width: 100vw;
+    background-position: center;
+    background-size: 37%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  ;`
 
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {
-          error ? <div>
-            <p className="error-text" >The provided credentials are incorrect</p>
-          </div> : null
-        }
-        <div className="flex-row flex-end">
-          <button type="submit">
-            Submit
-          </button>
-        </div>
-      </form>
-    </div>
+  const WhiteBackground = styled.div`
+    background-color: white; /* Black w/opacity/see-through */
+    border: 2px solid black;
+    border-radius: 10px;
+    color: black;
+    z-index: 2;
+    width: 80%;
+    padding: 20px;
+  ;`
+
+  return (
+    <StyledLoginBackground>
+      <div className="container my-1">
+        {/* <WhiteBackground>
+          <Link to="/signup">
+            ← Go to Signup
+          </Link>
+        </WhiteBackground> */}
+
+        <WhiteBackground>
+          <h2>Login</h2>
+          <form onSubmit={handleFormSubmit}>
+            <div className="flex-row space-between my-2">
+              <label htmlFor="email">Email address:</label>
+              <input
+                placeholder="youremail@test.com"
+                name="email"
+                type="email"
+                id="email"
+                onChange={handleChange}
+                />
+            </div>
+            <div className="flex-row space-between my-2">
+              <label htmlFor="pwd">Password:</label>
+              <input
+                placeholder="******"
+                name="password"
+                type="password"
+                id="pwd"
+                onChange={handleChange}
+                />
+            </div>
+            {
+              error ? <div>
+                <p className="error-text" >The provided credentials are incorrect</p>
+              </div> : null
+            }
+            <div className="flex-row flex-end">
+              <button type="submit">
+                Submit
+              </button>
+            </div>
+          </form>
+        </WhiteBackground>
+      </div>
+    </StyledLoginBackground>
   );
 }
 
