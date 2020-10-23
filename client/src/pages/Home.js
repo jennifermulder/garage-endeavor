1017bcdfec67975fd63d23e26e79ef68c3bd421f
sitakura <scot.itakura@gmail.com>
2020-10-22 22:04:11 -0700

    added buy sell trade buttons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,62 +5,154 @@ import garageDoor from "../assets/images/garage-door.png"
 import garageSign from "../assets/images/garage-sale-sign.gif"
 
 const StyledHomeBackground = styled.div`
-  position: fixed;
-  background-image: url(${openGarage});
-  height: 100%;
-  width: 100%;
-  background-size: 126vh;
-  background-position: center;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
+  position: absolute;
+  background-image: url(${openGarage});
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  height: 100%;
+  background-size: 100vh 100%;
+  background-position: center;
   background-repeat: repeat-x;
+  z-index: -1;
 ;`
 
 const GarageDoorDiv = styled.div`
-  position: relative;
+  position: absolute;
   background-image: url(${garageDoor});
-  top: -1%;
-  left: 0.7%;
-  width: 492px;
-  height: 276px;
-  background-size: 492px 276px;
+  background-size: 100% 100%;
+  height: 33vh;
+  width: 52vh;
   background-repeat: no-repeat;
-  -webkit-transition: background-position 1s ease;
-  -moz-transition: background-position 1s ease;
-  -ms-transition: background-position 1s ease;
-  -o-transition: background-position 1s ease;
   transition: background-position 1s ease;
-;`
-
-const CenteredSign = styled.div`
+  margin: 0.5vh 0 0 2vh;
+  z-index: 3;
   display: flex;
-  justify-content: center;
+  // flex-direction: column;
   align-items: center;
 ;`
 
-const GarageSign = styled.div`
-  position: fixed;
-  top: 18%;
+const GarageSign = styled.section`
   background-image: url(${garageSign});
   background-size: 100% 100%;
-  height: 144px;
-  width: 216px;
+  height: 17vh;
+  width: 27vh;
   background-repeat: no-repeat;
+  z-index: 2;
+  margin-top: 21vh;
+;`
+
+const Stylediv = styled.div`
+  background-size: 100% 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+;`
+
+const Buttondiv = styled.section`
+  // flex-direction: row;
+;`
+
+const RealButtondiv = styled.section`
+  position: relative;
+  // flex-direction: row;
+;`
+
+const Button = styled.button`
+  height: 8vh;
+  width: 16vh;
+  position: relative;
+  font-size: 1.2em;
+  margin: 0.7em;
+  padding: 0.25em 0.5em;
+  border: 2px solid;
+  border-radius: 3px;
   z-index: 1;
+;`
+
+const BuyButton = styled(Button)`
+  color: black;
+  border-color: black;
+  background-color: darkcyan;
+  margin-top: 25%; 
+;`
+
+const SellButton = styled(Button)`
+  color: black;
+  border-color: black;
+  background-color: darkcyan;
+  margin-top: 25%;
+;`
+
+const TradeButton = styled(Button)`
+  color: black;
+  border-color: black;
+  background-color: darkcyan;
+;`
+
+const RealButton = styled.button`
+  height: 8vh;
+  width: 16vh;
+  position: fixed;
+  font-size: 1.2em;
+  margin: 0.7em;
+  padding: 0.25em 0.5em;
+  border: 2px solid;
+  border-radius: 3px;
+  z-index: 1;
+;`
+
+const RealBuyButton = styled(RealButton)`
+  color: black;
+  border-color: black;
+  background-color: darkcyan;
+  margin-top: 25%; 
+;`
+
+const RealSellButton = styled(RealButton)`
+  color: black;
+  border-color: black;
+  background-color: darkcyan;
+  margin-top: 25%;
+;`
+
+const RealTradeButton = styled(RealButton)`
+  color: black;
+  border-color: black;
+  background-color: darkcyan;
 ;`
 
 const Home = () => {
   return (
     <div className="">
-      {/* <CenteredSign> */}
-        <GarageSign/>
-      {/* </CenteredSign> */}
-
       <StyledHomeBackground className="garage">
-        {/* <GarageDoorDiv>
-
-        </GarageDoorDiv> */}
+        <Stylediv>
+          <GarageSign/>
+          <Stylediv>
+            <Buttondiv href="#">
+              <BuyButton href="#">Buy</BuyButton>
+              <SellButton href="#">Sell</SellButton>
+              <Stylediv>
+                <TradeButton href="#">Trade</TradeButton>
+              </Stylediv>
+            </Buttondiv>
+            
+            <RealButtondiv>
+              <BuyButton href="#">Buy</BuyButton>
+              <SellButton href="#">Sell</SellButton>
+              <Stylediv>
+                <TradeButton href="#">Trade</TradeButton>
+              </Stylediv>
+            </RealButtondiv>
+            
+            <GarageDoorDiv>
+            </GarageDoorDiv>
+          </Stylediv>
+        </Stylediv>
       </StyledHomeBackground>
     </div>
   );
