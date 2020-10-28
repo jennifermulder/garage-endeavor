23cc9988df6fe2da44ce76be135e810ddb3b8a98
shhu21 <hu.sara21@gmail.com>
2020-10-28 13:37:32 -0700

    adds error message

--- client/src/pages/SellItem.js
+++ client/src/pages/SellItem.js
@@ -84,99 +84,100 @@ const StyledPhotoInput = styled.input`
   box-sizing: border-box;
 ;`
 
+const Error = styled.span`
+  display: none;
+  color: red;
+`;
+
 const SellItem = () => {
-    const [state, dispatch] = useStoreContext();
-    const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-    // console.log({data})
-    let { categories } = state;
-    // console.log({state})
-    const [ addProduct, {error} ] = useMutation(ADD_PRODUCT);
-    // const [ uploadImage ] = useMutation(UPLOAD_IMAGE);
-    let userID = '';
-    if(Auth.loggedIn()) {
-      userID = Auth.getProfile().data._id;
+  const [state, dispatch] = useStoreContext();
+  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+  const [ addProduct, {error} ] = useMutation(ADD_PRODUCT);
+  const { data, loading } = useQuery(QUERY_PRODUCTS);
+  
+  let userID = '';
+  if(Auth.loggedIn()) {
+    userID = Auth.getProfile().data._id;
+  }
+
+  const [formState, setFormState] = useState({ name: '', category: '', quantity: '', price: '', description: '', tag: '', image: '', user: userID });
+  let { categories } = state;
+  categories = categories.slice(0, 5);
+
+  useEffect(() => {
+    // if categoryData exists or has changed from the response of useQuery, then run dispatch()
+    if (categoryData) {
+      dispatch({
+          type: UPDATE_CATEGORIES,
+          categories: categoryData.categories
+      });
+      //save to idb store
+      categoryData.categories.forEach(category => {
+          idbPromise('categories', 'put', category);
+      });
     }
-    const [formState, setFormState] = useState({ name: '', category: '', quantity: '', price: '', description: '', tag: '', image: '', user: userID });
-    const { data, loading } = useQuery(QUERY_PRODUCTS);
-    categories = categories.slice(0, 5);
+    else if (!loading) {
+      idbPromise('categories', 'get').then(categories => {
+          dispatch({
+          type: UPDATE_CATEGORIES,
+          categories: categories
+          });
+      });
+    }
+  }, [categoryData, loading, dispatch]);
 
-    useEffect(() => {
-        // if categoryData exists or has changed from the response of useQuery, then run dispatch()
-        if (categoryData) {
-            dispatch({
-                type: UPDATE_CATEGORIES,
-                categories: categoryData.categories
-            });
-            //save to idb store
-            categoryData.categories.forEach(category => {
-                idbPromise('categories', 'put', category);
-            });
-        }
-        else if (!loading) {
-            idbPromise('categories', 'get').then(categories => {
-                dispatch({
-                type: UPDATE_CATEGORIES,
-                categories: categories
-                });
-            });
-        }
-    }, [categoryData, loading, dispatch]);
+  const handleFormSubmit = async event => {
+    event.preventDefault();
 
-    const handleFormSubmit = async event => {
-        event.preventDefault();
-
-        if(formState.name && formState.price && formState.category && formState.user) {
-            console.log({formState})
-
-            if (data) {
-                const newProduct = await addProduct({
-                    variables: {
-                      name: formState.name, 
-                      description: formState.description,
-                      tag: formState.tag,
-                      quantity: parseInt(formState.quantity),
-                      image: formState.image,
-                      price: parseInt(formState.price),
-                      category: formState.category,
-                      user: formState.user
-                    }
-                  }); 
-                console.log({newProduct});
-                dispatch({
-                  type: UPDATE_PRODUCTS,
-                  products: [...data.products, newProduct]
-                });
-            
-                idbPromise('products', 'put', newProduct.data.addProduct);
-
-                if(newProduct.data.addProduct.name) {
-                  window.location.assign(`/redirect?${newProduct.data.addProduct.name}`);
-                }
-            } 
-            else if (!loading) {
-                idbPromise('products', 'get').then((products) => {
-                  dispatch({
-                    type: UPDATE_PRODUCTS,
-                    products: products
-                  });
-                });
-            }
-        }
-        else{
-            //console.log("error");
-            // TODO: error handling for required fields
-        }
-    };
-
-    const handleChange = event => {
-        let { name, value } = event.target;
-        if(name === 'image') {
-          value = document.querySelector('#image').files[0];
-        }
+    if(formState.name && !isNaN(parseInt(formState.price)) && formState.category && formState.user && formState.image) {
+      if (data) {
+        const newProduct = await addProduct({
+          variables: {
+            name: formState.name, 
+            description: formState.description,
+            tag: formState.tag,
+            quantity: parseInt(formState.quantity),
+            image: formState.image,
+            price: parseInt(formState.price),
+            category: formState.category,
+            user: formState.user
+          }
+        }); 
         
-        setFormState({
-          ...formState,
-          [name]: value
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: [...data.products, newProduct]
+        });
+    
+        idbPromise('products', 'put', newProduct.data.addProduct);
+
+        if(newProduct.data.addProduct.name) {
+          window.location.assign(`/redirect?${newProduct.data.addProduct.name}`);
+        }
+      } 
+      else if (!loading) {
+        idbPromise('products', 'get').then((products) => {
+          dispatch({
+            type: UPDATE_PRODUCTS,
+            products: products
+          });
+        });
+      }
+    }
+    else {
+      document.getElementById('error-msg').style.display = 'block';
+    }
+  };
+
+  const handleChange = event => {
+    let { name, value } = event.target;
+    if(name === 'image') {
+      value = document.querySelector('#image').files[0];
+    }
+    
+    setFormState({
+      ...formState,
+      [name]: value
     });
   };
 
@@ -202,7 +203,6 @@ const SellItem = () => {
               <option value="" hidden>
                 Choose a category...
               </option>
-              {/* TODO: add a filter helper function to filter out duplicates */}
               {categories.map((category) => (
                 <option value={category._id}>{category.name}</option>
               ))}
@@ -228,7 +228,7 @@ const SellItem = () => {
               onChange={handleChange}
             />
             <label>Item Tag</label>
-            <textarea 
+            <StyledTextArea 
                 name='tag'
                 placeholder='Add a tag'
                 onChange={handleChange}
@@ -240,6 +240,7 @@ const SellItem = () => {
               type="file"
               onChange={handleChange}
             />
+            <Error id='error-msg'>*Either a required field is missing or information has been inputted incorrectly.  Please review your information.</Error>
             <div className="flex-row flex-end">
               <button type="submit">Add Listing</button>
             </div>
