1d5708212ed252575c71bb73e62a85da5619906f
shhu21 <hu.sara21@gmail.com>
2020-10-28 13:56:34 -0700

    adds countdown to redirect

--- client/src/pages/Redirect.js
+++ client/src/pages/Redirect.js
@@ -7,16 +7,26 @@ function Redirect() {
         return url;
     };
 
-    // setTimeout(function(){ window.location.assign('buy') }, 3000);
+    function cntDown (cnt) {
+        if(cnt !== 0) {
+            return setInterval(function () {
+                document.getElementById('cnt').textContent = `${cnt} seconds...`;
+                cnt -= 1;
+                if(cnt === 0) {
+                    window.location.href = '/buy';
+                }
 
+            }, 1000);
+        }
+    }
+
+    cntDown(10);
     return (
         <div>
             <h1>Your Listing ({listingName()}) was successfully added!</h1>
+            <h3>The page will automatically redirect to browse products in <span id='cnt'></span></h3>
             <a href='/sell-item'>
-                <h2>Add another listing</h2>
-            </a>
-            <a href='buy'>
-                <h2>Browse listings</h2>
+                <h2><span><img src="https://img.icons8.com/ultraviolet/40/000000/circled-left-2.png"/></span>Add another listing</h2>
             </a>
         </div>
     )
